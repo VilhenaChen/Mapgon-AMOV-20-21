f15cc9200de791e674793a404b9776c499d39784
Diogo Vilhena <diogodevilhena@sapo.pt>
2021-01-07 20:34:48 +0000

    First designs of the activities

---
+++ app/src/main/java/pt/vilhena/mapgon/atividades/DefineEquipa.kt
@@ -0,0 +1,13 @@
+package pt.vilhena.mapgon.atividades
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pt.vilhena.mapgon.R
+
+class DefineEquipa : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_define_equipa)
+    }
+}
